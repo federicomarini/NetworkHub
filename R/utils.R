b8bd5a0b9cad451c5453a030b29ad0bd8b8aa7a9
Federico Marini <marinif@uni-mainz.de>
2023-02-14 10:44:08 +0100

    caching to, and fetching from - individual functions to be used from the other methods

--- R/utils.R
+++ R/utils.R
@@ -18,3 +18,86 @@ initialize_NetworkHub <- function(nh_cachedir = "NetworkHub") {
   
   return(bfc_nh)
 } 
+
+
+# internal functions called by the more specific ones ---------------------
+
+#' Title
+#'
+#' @param rname 
+#' @param fpath 
+#' @param nh_cachedir 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # for example, retrieve something from stringDB
+#' # "https://stringdb-static.org/download/protein.links.full.v11.5/9606.protein.links.full.v11.5.txt.gz"
+#' cache_NetworkHub(rname = "STRINGDB_Homo sapiens_v11.5", 
+#'                  fpath = "https://stringdb-static.org/download/protein.links.full.v11.5/9606.protein.links.full.v11.5.txt.gz")
+cache_NetworkHub <- function(rname, 
+                             fpath, 
+                             nh_cachedir = "NetworkHub", 
+                             ...)
+{
+  cache_dir <- tools::R_user_dir(nh_cachedir, which = "cache")
+  bfc_nh <- BiocFileCache::BiocFileCache(cache_dir)
+  
+  # check if fpath is being tracked 
+  nh_query <-  BiocFileCache::bfcquery(bfc_nh, fpath)
+  if (BiocFileCache::bfccount(nh_query) == 0) {
+    rpath <- BiocFileCache::bfcadd(bfc_nh, rname, fpath, ...) 
+  } else {
+    rpath <- nh_query$rpath
+  }
+  return(rpath)
+}
+
+
+
+#' Title
+#'
+#' @param rname 
+#' @param update 
+#' @param nh_cachedir 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' fetch_NetworkHub(rname = "STRINGDB_Homo sapiens_v11.5")
+#' 
+#' fetch_NetworkHub(rname = "STRINGDB_Homo sapiens_v11.0")
+fetch_NetworkHub <- function(rname, 
+                             update = TRUE,
+                             nh_cachedir = "NetworkHub",
+                             ...)
+{
+  cache_dir <- tools::R_user_dir(nh_cachedir, which = "cache")
+  bfc_nh <- BiocFileCache::BiocFileCache(cache_dir)
+  
+  nh_query <-  BiocFileCache::bfcquery(bfc_nh, rname, exact = TRUE)
+  
+  # is there already a cached version?
+  res_nh <- NULL
+  if (BiocFileCache::bfccount(nh_query))
+  {
+    rid <- nh_query$rid
+    
+    # is the cached version outdated?
+    nu <- FALSE
+    if (update) 
+      nu <- BiocFileCache::bfcneedsupdate(bfc_nh, rid)
+    if (!isFALSE(nu)) 
+      BiocFileCache::bfcdownload(bfc_nh, rid, ask = FALSE, ...)
+    
+    message("Using cached version from ", nh_query$create_time)
+    res_nh <- BiocFileCache::bfcrpath(bfc_nh, rname)
+  }
+  if (is.null(res_nh))
+    message("No record found in NetworkHub!")
+  return(res_nh)   
+}
