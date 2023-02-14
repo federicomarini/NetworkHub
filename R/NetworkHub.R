3c48f4a6f6cf60bdde87430f82bbc292c16fb80b
Federico Marini <marinif@uni-mainz.de>
2023-02-14 13:12:39 +0100

    likely the main function to be used later on - will have to handle the dispatches cleverly

---
+++ R/NetworkHub.R
@@ -0,0 +1,22 @@
+# main workhorse, will do some clever dispatch ----------------------------
+
+#' Title
+#'
+#' @param network_source 
+#' @param species 
+#' @param version 
+#' @param cache 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_networkdata <- function(network_source,
+                            species,
+                            version,
+                            cache = TRUE) {
+  # do the right dispatches...
+  
+}
+
+
