0b8b8feee6cc06e8464bf40aa7cde94590577fef
Federico Marini <marinif@uni-mainz.de>
2023-02-26 15:51:33 +0100

    some notes about retrieval from intact added

--- R/intact.R
+++ R/intact.R
@@ -1,2 +1,15 @@
-
 # handling data retrieval from IntAct ------------------------------------------
+
+# retrieve from here
+# https://ftp.ebi.ac.uk/pub/databases/intact/current/psimitab/
+  
+# extract and create
+# careful more than one file, and large - gets truncated!!
+
+# in theory, would go with
+# vroom::vroom(unz("~/Downloads/intact.zip", "intact.txt"))
+
+# but cuts it to 995,802 × 42
+
+# and it should be 
+# 1194447 x 42
